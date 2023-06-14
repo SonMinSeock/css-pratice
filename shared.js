0b1f3ae279c15f151e9e39eebb7709a9d3e27f18
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-06-14 18:58:08 +0900

    button click event handler defined, modal 기능 켜기 완료

--- shared.js
+++ shared.js
@@ -1,3 +1,10 @@
 const backdrop = document.querySelector(".backdrop");
+const modal = document.querySelector(".modal");
+const selectPlanButtons = document.querySelectorAll(".plan button");
 
-backdrop.style.display = "block";
+selectPlanButtons.forEach((selectPlanButton) =>
+  selectPlanButton.addEventListener("click", (event) => {
+    backdrop.style.display = "block";
+    modal.style.display = "block";
+  })
+);
