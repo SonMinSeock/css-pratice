f585d0f81b86fb46c0288db8ae7fbf1d2bbc9567
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-06-14 19:12:51 +0900

    Open Modal and Close Modal Finishl

--- shared.js
+++ shared.js
@@ -1,10 +1,30 @@
 const backdrop = document.querySelector(".backdrop");
 const modal = document.querySelector(".modal");
 const selectPlanButtons = document.querySelectorAll(".plan button");
+const modalNoButton = document.querySelector(
+  ".modal__actions .modal__action--negative"
+);
+
+function openModal() {
+  backdrop.style.display = "block";
+  modal.style.display = "block";
+}
+
+function closeModal() {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
 
 selectPlanButtons.forEach((selectPlanButton) =>
   selectPlanButton.addEventListener("click", (event) => {
-    backdrop.style.display = "block";
-    modal.style.display = "block";
+    openModal();
   })
 );
+
+backdrop.addEventListener("click", () => {
+  closeModal();
+});
+
+modalNoButton.addEventListener("click", () => {
+  closeModal();
+});
